7177a331d44ab420710a844ab1fde22e9bf4845f
oluwakayode <kaylove4real10@gmail.com>
2022-03-02 09:39:02 +0100

    ignore build folder

--- build/utils/interface.util.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NodeEnv = void 0;
-var NodeEnv;
-(function (NodeEnv) {
-    NodeEnv["PRODUCTION"] = "production";
-    NodeEnv["DEVELOPMENT"] = "development";
-    NodeEnv["STAGING"] = "staging";
-})(NodeEnv = exports.NodeEnv || (exports.NodeEnv = {}));
